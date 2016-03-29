e92935dcb99f0efff2e059ba0febecd3e768cd8a
Brian Yarsawich <byarsawich@Brians-MacBook-Pro.local>
2016-03-29 08:51:38 -0400

    code for oauth2 gem is there, it's just not working

--- app/controllers/oauth_controller.rb
+++ app/controllers/oauth_controller.rb
@@ -6,5 +6,28 @@ class OauthController < ApplicationController
   end
 
   def create
+    session_code = request.env['rack.request.query_hash']['code']
+
+    client = Rack::OAuth2::Client.new(
+      identifier: ENV["GITHUB_OAUTH_FUN_KEY"],
+      secret: ENV["GITHUB_OAUTH_FUN_SECRET_KEY"],
+      redirect_uri: 'http://localhost:3000/auth/github/callback',
+      code: session_code,
+      host: 'github.com',
+      authorization_endpoint: '/login/oauth/authorize',
+      token_endpoint: '/login/oauth/access_token'
+    )
+
+    authorization_uri = client.authorization_uri(
+      scope: [:profile, :email],
+      state: session[:state],
+      response_type: :token
+    )
+
+    open "#{authorization_uri}"
+
+    byebug
+    access_token = client.access_token!
+
   end
 end
--- app/controllers/restclient_oauth_controller.rb
+++ app/controllers/restclient_oauth_controller.rb
@@ -5,13 +5,13 @@ class RestclientOauthController < ApplicationController
   def show
     # fetch user information
     @auth_result = JSON.parse(RestClient.get('https://api.github.com/user',
-        {:params => {:access_token => session[:access_token]}}))
+        {params: {access_token: session[:access_token]}}))
 
     # if the user authorized it, fetch private emails
     has_user_email_scope = session[:scopes].include? 'user:email'
     if has_user_email_scope
       @auth_result['private_emails'] = JSON.parse(RestClient.get('https://api.github.com/user/emails',
-          {:params => {:access_token => session[:access_token]}}))
+          {params: {access_token: session[:access_token]}}))
     end
   end
 
@@ -21,10 +21,10 @@ class RestclientOauthController < ApplicationController
 
    #post it back to github
    result = RestClient.post('https://github.com/login/oauth/access_token',
-        {:client_id => ENV["GITHUB_OAUTH_FUN_KEY"],
-         :client_secret => ENV["GITHUB_OAUTH_FUN_SECRET_KEY"],
-         :code => session_code},
-         :accept => :json)
+        {client_id: ENV["GITHUB_OAUTH_FUN_KEY"],
+         client_secret: ENV["GITHUB_OAUTH_FUN_SECRET_KEY"],
+         code: session_code},
+         accept: :json)
 
     # extract the token and granted scopes
     access_token = JSON.parse(result)['access_token']
